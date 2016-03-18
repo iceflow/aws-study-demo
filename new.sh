ac42efe796600e9d4b0406ced1ab2a98d8f1b800
Leo Chen <chenlt78@qq.com>
2016-03-18 04:40:20 +0000

    Remove unneeded info

--- new.sh
+++ new.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-#./launch_linux.sh QD011_Prod_ZXDB_001 ami-52d1183f 168.161.129.100 t2.micro test AppZLTHangQing BP1_B_C_011_Cache001 BP1_B_C_QD011_Cache001
-./launch_linux.sh QD011_Prod_ZXDB_004 ami-52d1183f 168.161.129.104 t2.micro test AppZLTHangQing BP1_B_C_011_Cache001 BP1_B_C_QD011_Cache001
+./launch_linux.sh EC2_Name ami-52d1183f 192.168.129.104 t2.micro testkey test_role test_subnet test_sg
 
 
 
