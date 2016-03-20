fd4370bb1ff7f41631093ffdb64e1fe435a1a034
Leo Chen <chenlt78@qq.com>
2016-03-20 04:31:51 +0000

    Normalize launch scripts
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- new.sh
+++ new.sh
@@ -4,7 +4,7 @@
 ./launch_linux.sh EC2_Name ami-52d1183f 192.168.129.104 t2.micro testkey test_role test_subnet test_sg
 
 # Launch Windows 2012 instance`
-./launch_windows.sh QD011_Prod_ZXDB_004 ami-cbca01a6 168.161.129.120 t2.micro test AppZLTHangQing BP1_B_C_011_Cache001 BP1_B_C_QD011_Cache001
+./launch_windows.sh EC2_Name ami-52d1183f 192.168.129.104 t2.micro testkey test_role test_subnet test_sg
 
 
 
