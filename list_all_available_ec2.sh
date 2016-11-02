a38c8d6c876e3ece7ff3e3d9fd7d5cd5ef7ce002
Leo Chen <chenlt78@qq.com>
2016-11-02 13:27:42 +0800

    Adding profile switch
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- list_all_available_ec2.sh
+++ list_all_available_ec2.sh
@@ -4,13 +4,11 @@
 REGION_LIST="cn-north-1"
 
 
-#PROFILE=$1
+PROFILE=$1
+
+[ "_$PROFILE" = "_" ] && PROFILE=default
 
 for R in ${REGION_LIST}; do
-        #if [ "_$PROFILE" != "_" ]; then
-        #else
-        #        aws ec2 --region $R describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, InstanceType]' --filter Name=instance-state-code,Values=16 --output text 
-        #fi
 		echo "AvailabilityZone,State,InstanceId,InstanceType,Name"
-		aws $PROFILE ec2 --region $R describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, InstanceType, Tags[0].Value]' --filter Name=instance-state-code,Values=16,80 --output text | awk '{print $1","$2","$3","$4","$5}'
+		aws --profile $PROFILE ec2 --region $R describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, InstanceType, Tags[0].Value]' --filter Name=instance-state-code,Values=16,80 --output text | awk '{print $1","$2","$3","$4","$5}'
 done
--- list_all_ebs.sh
+++ list_all_ebs.sh
@@ -7,13 +7,9 @@ REGION_LIST="cn-north-1"
 
 PROFILE=$1
 
-for R in ${REGION_LIST}; do
-	#if [ "_$PROFILE" != "_" ]; then
-	#aws --profile $1 ec2 --region $R describe-volumes --query 'Volumes[].[AvailabilityZone,VolumeId,State,Iops,Size]' --filter Name=status,Values=available --output text
-	#else
-	#aws ec2 --region $R describe-volumes --query 'Volumes[].[AvailabilityZone,VolumeId,State,Iops,Size]' --filter Name=status,Values=available --output text
-	#fi
+[ "_$PROFILE" = "_" ] && PROFILE=default
 
+for R in ${REGION_LIST}; do
 	echo "AvailabilityZone,VolumeId,State,Iops,Size,VolumeType"
-	aws ec2 $PROFILE describe-volumes --query 'Volumes[].[AvailabilityZone,VolumeId,State,Iops,Size,VolumeType]' --output text | awk '{print $1","$2","$3","$4","$5","$6}'
+	aws ec2 --profile $PROFILE describe-volumes --query 'Volumes[].[AvailabilityZone,VolumeId,State,Iops,Size,VolumeType]' --output text | awk '{print $1","$2","$3","$4","$5","$6}'
 done
