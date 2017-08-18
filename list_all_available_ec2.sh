9e1b7d6c642969fb6eb33f24e119d27a91176906
Leo Chen <chenlt78@qq.com>
2017-08-18 15:17:01 +0800

    Adding EC2 platform column
    Signed-off-by: Leo Chen <chenlt78@qq.com>

--- list_all_available_ec2.sh
+++ list_all_available_ec2.sh
@@ -9,6 +9,6 @@ PROFILE=$1
 [ "_$PROFILE" = "_" ] && PROFILE=default
 
 for R in ${REGION_LIST}; do
-		echo "AvailabilityZone,State,InstanceId,InstanceType,Name"
-		aws --profile $PROFILE ec2 --region $R describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, InstanceType, Tags[0].Value]' --filter Name=instance-state-code,Values=16,80 --output text | awk '{print $1","$2","$3","$4","$5}'
+		echo "AvailabilityZone,State,InstanceId,InstanceType,Platform,Name"
+		aws --profile $PROFILE ec2 --region $R describe-instances --query 'Reservations[*].Instances[*].[Placement.AvailabilityZone, State.Name, InstanceId, InstanceType, Platform, Tags[0].Value]' --filter Name=instance-state-code,Values=16,80 --output text | awk '{print $1","$2","$3","$4","$5}'
 done
